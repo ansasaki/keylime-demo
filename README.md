c5990263c3bb08ecaa50973168d5bab98ec0da6a
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2025-04-16 16:00:53 +0200

    Initial version of the demo
    - Create and configure the monitored nodes running the Keylime agent
      - This includes the virtual network setup for the agents in the
        monitored nodes to have access to the services running on the host
    - Download, configure, and deploy the Keylime verifier and registrar
      containers
    
    The policy generation and enrollment of agents to the verifier is not
    implemented yet.
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- README.md
+++ README.md
@@ -1,2 +1,176 @@
 # keylime-demo
 Automation to deploy a Keylime demo
+
+# Instructions
+
+## Create an account in quay.io
+
+This is necessary to download the Keylime verifier and registrar images provided
+there.
+
+Then, you can create a token to authenticate to quay.io when using the demo by:
+
+1. Click on your avatar on the upper right corner and `Account Settings`
+2. Click on the robot icon (Robot accounts) on the left bar
+3. Createa token by clicking on the `Create Robot Account` button on the right
+4. After selecting the name for the robot account and a desciption, click on
+   `Create robot account` button
+5. Select the repositories the robot account will have access (none are required
+   for the demo), then click on `Close` button
+6. Click on the cog icon on the right of the newly created bot account and `View
+   Credentials`
+7. Copy the Robot Account token
+8. (Recommended) create a podman secret containing your newly created
+   authentication token by running:
+   ```
+   echo -n "<YOUR_ROBOT_TOKEN>" | podman secret create keylimeDemoPassword
+   ```
+
+## Create your inventory
+
+Use the provided example `inventory.yml.example` to create your inventory for
+the demo:
+
+- Copy the example and rename to `inventory.yml`:
+  ```
+  cp inventory.yml.example inventory.yml
+  ```
+- Set your robot quay.io name as the username by replacing `YOUR_USERNAME` with
+    your robot account username (it should be something like `username+robot`)
+- Set you quay.io registry secret that contains you password by replacing
+    `YOUR_SECRET` with the podman secret you created above (e.g.
+    `keylimeDemoPassword`)
+- Choose a directory where the necessary files will be created for the demo in
+    the `demo_dir` fields. The default is `~/keylime_demo`. Beware: the demo
+    creates a new VM for each monitored node (by default 2 nodes named `node1`
+    and `node2`). The directory where the demo files are stored needs to have
+    enough space (normally creating a directory in `/tmp` is **not** a good idea
+    because the available space depends on the memory size)
+- You can create mode monitored nodes by adding to the `monitored` following the
+    model used for `node1` and `node2`. Keep the ansible_host as the node name
+    followed by `.demo` domain as we create a virtual network for them and want
+    to address the nodes by hostname (e.g. `node1.demo`)
+
+## Run the playbook to create the demo
+
+The demo will download and deploy the Keylime verifier and registrar container
+images. For the monitored nodes, the demo will create a new VM for each node,
+place them under a virtual NAT network (by default in subnet `192.168.42.0/24`),
+setup the firewall for communicaton between guest VMs and host, setup DNS
+resolution, and install the Keylime agent in each node, configuring the IPs and
+certificates.
+
+The playbook accesses the `localhost` using `ssh`. Make sure the `sshd` service
+is running:
+
+```
+systemctl start sshd
+```
+
+Then, run the playbook:
+```
+ansible-playbook -i inventory.yml demo.yml --ask-become-pass
+```
+
+The `--ask-become-pass` is necessary to prompt for the `sudo` password. It will
+be used to setup the network for the demo.
+
+# Details on the changes made by the demo to the controller (local) machine
+
+The demo pulls the Keylime verifier and registrar containers and run it, mapping
+the host ports to the container ports for communication. Inside the container,
+they are configured to listen to the IP `0.0.0.0`, meaning that they will accept
+any incoming requests regardless of the target IP.
+
+The demo also creates a VM for each monitored node using the latest Fedora
+Server stable image. The VM is created with UEFI boot and a virtual TPM.
+
+The network configuration is modified in a quasi-temporary manner, meaning that
+the few permanent changes are left, but they are benign. In the following
+section we will list what is modified and how to undo the changes.
+
+The changes made by the demo playbook are idempotent.
+
+### Virtual bridge network
+
+A virtual bridge network interface is created as part of the demo network
+configuration.
+
+The virtual network definition is permanent, but the created network is
+destroyed on reboot.
+
+You can check the created network by running:
+
+```
+sudo virsh net-list --all
+```
+
+By default, a `demo` network is created. To destroy it, run:
+
+```
+sudo virsh net-destroy --network demo
+```
+
+To undefine it , run:
+
+```
+sudo virsh net-undefine --network demo
+```
+
+You can also check the virtual bridge interface created by running:
+
+```
+ip link show type bridge
+```
+
+By default, a virtual interface called `virbr-demo` is created. This interface
+is destroyed on reboot or if the `virsh net-destroy --network demo` command is
+executed.
+
+### Firewall
+
+We run the Keylime verifier and registrar services as containers on the host
+network, mapping the necessary ports for communication.
+Since the monitored nodes are running on VMs under a NAT network, it is
+necessary to modify the firewall configuration to allow the requests from inside
+the guest VM to reach the service in the host.
+
+This is done my adding a rich rule to the `libvirt` zone in `firewalld`
+configuration. This change is temporary and undone on reboot.
+
+The configuration runs the following command to add the rich rule that allows
+communication coming from the NAT subnet to the host:
+
+```
+sudo firewall-cmd --zone=libvirt --add-rich-rule='
+    rule family="ipv4"
+        source address="192.168.42.0/24"
+        accept'
+```
+
+Where the `192.168.42.0/24` is the default virtual NAT network where the
+monitored nodes are connected.
+
+### DNS
+
+A `systemd-resolved` configuration snippet is added to forward the hostname
+resolution for the `*.demo` domain to the `libvirt` `dnsmasq` instance created
+to handle the virtual NAT network.
+
+The following snippet is placed in
+`/etc/systemd/resolved.conf.d/resolved-demo.conf`:
+
+```
+[Resolve]
+DNS=192.168.42.1
+Domains=~demo
+```
+
+This is done to allow the monitored nodes to be addressed by their hostname
+(e.g. `node1.demo`)
+
+This change is permanent. To undo, remove the snippet by running:
+
+```
+sudo rm /etc/systemd/resolved.conf.d/resolved-demo.conf
+```
---
+++ roles/create_vms/files/create_vm.sh
@@ -0,0 +1,276 @@
+#!/bin/bash
+
+DVD="fedora.iso"
+IP="dhcp"
+NAME="keylime"
+NETWORK_BRIDGE="virbr0"
+OS_VARIANT="fedora"
+OS_VERSION="-unknown"
+OUT_DIR="$(realpath .)"
+PASSWORD="keylime"
+ROOT_PASSWORD="keylime"
+SSH_KEY="$(realpath ~/.ssh/id_rsa.pub)"
+USER="admin"
+
+usage() {
+    echo "usage: $0 [OPTIONS]"
+    echo "Options: "
+    echo "    [-a | --address] <IP>             : Set a static IP address (default: 'dhcp')"
+    echo "    [-b | --bridge] <NETWORK_BRIDGE>  : Set the network bridge to attach the VM (default: 'virbr0')"
+    echo "    [-d | --debug]                    : Print debug messages"
+    echo "    [-h | --help]                     : Print this usage message"
+    echo "    [-i | --image] <IP>               : Set the installation ISO image (default: 'fedora.iso')"
+    echo "    [-k | --ssh-key] <PATH_TO_PUBKEY> : Add the SSH public key to the VM 'authorized_keys' (default: ~/.ssh/id_rsa.pub)"
+    echo "    [-n | --name | --vm] <NAME>       : Name of the VM (default: 'keylime')"
+    echo "    [-o | --outdir] <OUTPUT_DIR>      : Set the output directory (default: '.')"
+    echo "    [-p | --password] <PASSWORD>      : Set the user password (default: 'keylime')"
+    echo "    [-r | --root-pw] <ROOT_PASSWORD>  : Set the root user password (default: 'keylime')"
+    echo "    [-s | --os] <OS_VARIANT>          : Name of the OS variant (default: 'fedora')"
+    echo "    [-u | --user] <USER>              : Add the given user (default: 'admin')"
+    echo "    [-v | --version] <OS_VERSION>     : OS version number (default: '-unknown')"
+}
+
+debug() {
+    if [[ ! -z "${VERBOSE}" ]]; then
+        echo "$1"
+    fi
+}
+
+while [ "$#" -gt 0 ]; do
+    arg="$1"
+
+    case $arg in
+        -h | --help)
+            usage
+            exit 0
+            ;;
+        -a | --address)
+            IP="$2"
+            shift
+            ;;
+        -a=* | --address=*)
+            IP=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        -b | --bridge)
+            NETWORK_BRIDGE="$2"
+            shift
+            ;;
+        -b=* | --bridge=*)
+            NETWORK_BRIDGE=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        -d | --debug)
+            VERBOSE="True"
+            ;;
+        -i | --image)
+            DVD="$2"
+            shift
+            ;;
+        -i=* | --image=*)
+            DVD=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        -k | --ssh-key)
+            SSH_KEY="$2"
+            shift
+            ;;
+        -k=* | --ssh-key=*)
+            SSH_KEY=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        -n | --name | --vm)
+            NAME="$2"
+            shift
+            ;;
+        -n=* | --name=* | --vm=*)
+            NAME=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        -o | --outdir)
+            OUT_DIR="$2"
+            shift
+            ;;
+        -o=* | --outdir=*)
+            OUT_DIR=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        -p | --password)
+            PASSWORD="$2"
+            shift
+            ;;
+        -p=* | --password=*)
+            PASSWORD=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        -r | --root-pw)
+            ROOT_PASSWORD="$2"
+            shift
+            ;;
+        -r=* | --root-pw=*)
+            ROOT_PASSWORD=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        -s | --os)
+            OS_VARIANT="$2"
+            shift
+            ;;
+        -s=* | --os=*)
+            OS_VARIANT=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        -u | --user)
+            USER="$2"
+            shift
+            ;;
+        -u=* | --user=*)
+            USER=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        -v | --version)
+            OS_VERSION="$2"
+            shift
+            ;;
+        -v=* | --version=*)
+            OS_VERSION=$(echo $arg | cut -d '=' -f 2)
+            ;;
+        *)
+            echo "unknown option '$1'"
+            usage
+        ;;
+    esac
+    shift
+done
+
+echo "Creating VM \"${NAME}\""
+
+debug "Name = ${NAME}"
+debug "Image = ${DVD}"
+debug "IP = ${IP}"
+debug "BRIDGE = ${NETWORK_BRIDGE}"
+debug "SSH_KEY = ${SSH_KEY}"
+debug "PASSWORD = ${PASSWORD}"
+debug "OS_VARIANT = ${OS_VARIANT}"
+debug "OS_VERSION = ${OS_VERSION}"
+debug "OUT_DIR = ${OUT_DIR}"
+debug "ROOT_PASSWORD = ${ROOT_PASSWORD}"
+debug "USER = ${USER}"
+BASE_DIR="$NAME"
+
+if [[ ! -d "${OUT_DIR}" ]]; then
+    # Check that the output directory exists
+    echo "The output directory '${OUT_DIR}' does not exist or is not a directory"
+    exit 1
+fi
+
+CRYPT_PW=$(python -c "import crypt;print(crypt.crypt('${PASSWORD}'));")
+debug "CRYPT_PW = ${CRYPT_PW}"
+
+CRYPT_ROOT_PW=$(python -c "import crypt;print(crypt.crypt('${ROOT_PASSWORD}'));")
+debug "CRYPT_ROOT_PW = ${CRYPT_ROOT_PW}"
+
+if [[ "${IP}" != "dhcp" ]]; then
+    debug "Static IP will be set as ${IP}"
+    NET="network --onboot=yes --device=link --bootproto static --ip ${IP} --netmask 255.255.255.0 --gateway 192.168.124.1 --nameserver 192.168.124.1 --activate"
+else
+    debug "Static IP not set"
+    NET="network --bootproto=dhcp"
+fi
+
+if [[ -n "${SSH_KEY}" ]]; then
+    # Check if the file looks like a private key
+    if (grep "^-*BEGIN.*PRIVATE" "${SSH_KEY}"); then
+        echo "'${SSH_KEY}' does not look like a public SSH key, but a private key. Aborting."
+        exit 1
+    fi
+    # Otherwise check if ss-keygen can parse and find the public key
+    if (ssh-keygen -l -f ${SSH_KEY} >> /dev/null); then
+        PUB_KEY="$(cat ${SSH_KEY})"
+        debug "PUB_KEY=${PUB_KEY}"
+    else
+        echo "'${SSH_KEY}' does not look like a public SSH key. Aborting."
+        exit 1
+    fi
+fi
+
+cat << EOF > "${OUT_DIR}/${NAME}.ks"
+# Language and locale configuration
+firstboot --disable
+lang en_US.UTF-8
+keyboard --xlayouts='us'
+timezone America/New_York --utc
+
+# Set root password and create user for management with ansible
+rootpw ${CRYPT_ROOT_PW} --iscrypted
+user --name ${USER} --password=${CRYPT_PW} --iscrypted
+
+reboot
+text
+skipx
+
+# Create disks partitions
+ignoredisk --only-use=vda
+clearpart --all --initlabel --disklabel=gpt --drives=vda
+part /boot/efi --size=512 --fstype=efi
+part /boot --size=512 --fstype=xfs --label=boot
+part / --fstype="xfs" --ondisk=vda --label=root --grow
+
+${NET}
+network --hostname=${NAME}
+
+firstboot --disable
+selinux --enforcing
+firewall --enabled --http --ssh
+
+reboot
+
+%packages
+@^server-product-environment
+python3-cryptography
+%end
+
+%post
+LOG_FILE=/root/ks-post.log
+[ -n "" ] && LOG_FILE=/dev/null
+exec < /dev/tty3 > /dev/tty3
+chvt 3
+(
+    IP="$IP"
+    if [ "${IP}" != "dhcp" ]; then
+        mkdir -p /etc/NetworkManager/conf.d/
+        cat << _EOF >> /etc/NetworkManager/conf.d/00-10mt-static-ip.conf
+# File automatically created when setting up a VM with static IP address.
+[device]
+keep-configuration=no
+allowed-connections=except:origin:nm-initrd-generator
+_EOF
+    fi
+
+    SSH_KEY="$PUB_KEY"
+    if [ -n "${PUB_KEY}" ]; then
+        # Copy public ssh key.
+        mkdir -m0700 -p /root/.ssh/
+        echo "${PUB_KEY}" > /root/.ssh/authorized_keys
+        chmod 0600 /root/.ssh/authorized_keys
+        restorecon -R /root/.ssh/
+    fi
+    if [ -n "${PUB_KEY}" ]; then
+        # Copy public ssh key.
+        mkdir -m0700 -p /home/${USER}/.ssh/
+        echo "${PUB_KEY}" > /home/${USER}/.ssh/authorized_keys
+        chown -R ${USER}:${USER} /home/${USER}/.ssh
+        chmod 0600 /home/${USER}/.ssh/authorized_keys
+        restorecon -R /home/${USER}/.ssh/
+    fi
+
+) 2>&1 | tee "${LOG_FILE}" > /dev/tty3
+chvt 1
+%end
+EOF
+
+# Install the VM using the created kickstart file
+virt-install --connect qemu:///session \
+    --name ${NAME} \
+    --os-variant ${OS_VARIANT}${OS_VERSION} \
+    --ram 2048 --vcpus 2 --disk path="${OUT_DIR}/${NAME}.qcow2",size=20,bus=virtio,cache=none,format=qcow2 \
+    --location ${DVD} \
+    --initrd-inject "${OUT_DIR}/${NAME}.ks" \
+    --extra-args "inst.ks=file:/${NAME}.ks disable_ipv6=1 console=tty0 console=ttyS0,115200 rhgb quiet" \
+    --nographic \
+    --noautoconsole \
+    --wait -1 \
+    --rng /dev/urandom \
+    --tpm backend.type=emulator,backend.version=2.0,model=tpm-tis \
+    --console pty,target_type=serial \
+    --network bridge=${NETWORK_BRIDGE} \
+    --boot uefi
